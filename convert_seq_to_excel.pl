390559c14dbb78049997206aa8efadc71621b762
Matthew Berginski <matthew.berginski@gmail.com>
2019-10-18 11:33:04 -0400

    adding combined reads file and script to build

--- convert_seq_to_excel.pl
+++ convert_seq_to_excel.pl
@@ -5,18 +5,47 @@ use File::Slurp;
 use Class::CSV;
 use File::Basename;
 
-my @seq_files = <seq_files/*>;
+my @seq_files = <plasmid_sequence/*>;
+my @read_files = <read_files/*>;
+
+#build up a quick hash of the file numbers in order to map from the file numbers
+#in the plasmid sequence files to the associated read files
+my %read_file_nums;
+my %read_file_full_file;
+for (@read_files) {
+	if ($_ =~ /(\d+)_/) {
+		$read_file_nums{int($1)} = fileparse($_);
+		$read_file_full_file{int($1)} = $_;
+	} else {
+		die "Error finding file number: $_";
+	}
+}
+
 
 my $output_csv = Class::CSV->new(
-	fields => [qw/filename sequence/]
+	fields => [('Seq Name', 'Full seq', 'Read 1 name', 'Read 1 seq', 'Read 2 name', 'Read 2 seq')]
 );
 
 for (@seq_files) {
 	my $filename = fileparse($_);
-	$output_csv->add_line([$filename, get_file_contents($_)]);
+	my $file_num;
+	if ($filename =~ /(\d+)\./) {
+		$file_num = int($1);
+	} else {
+		die "Error finding file number: $_";
+	}
+
+
+	$output_csv->add_line([$filename, 
+			get_file_contents($_),
+			$read_file_nums{$file_num*2-1},
+			get_file_contents($read_file_full_file{$file_num*2-1}),
+			$read_file_nums{$file_num*2},
+			get_file_contents($read_file_full_file{$file_num*2}),]
+		);
 }
 
-print "filename,sequence\n";
+print "'Seq Name', 'Full seq', 'Read 1 name', 'Read 1 seq', 'Read 2 name', 'Read 2 seq'\n";
 $output_csv->print();
 
 sub get_file_contents {
